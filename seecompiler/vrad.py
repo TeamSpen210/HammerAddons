cfca512d0c3c8db6c15602e668c49449d135ea56
TeamSpen210 <spencerb21@live.com>
2017-11-13 17:25:36 +1000

    Run packlist module
    Transferred from TeamSpen210/srctools @ d2d829180bfd7cb8133e3f3c5b9d1b7b094add6b

--- seecompiler/vrad.py
+++ seecompiler/vrad.py
@@ -1,4 +1,6 @@
 """Replaces VRAD, to run operations on the final BSP."""
+from zipfile import ZipFile
+
 from seecompiler.logger import init_logging
 from seecompiler.transformation import run_transformations
 
@@ -6,9 +8,12 @@ LOGGER = init_logging('seecompiler/vrad.log')
 
 import sys
 import os
+import io
 
-from srctools.bsp import BSP
+from srctools.bsp import BSP, BSP_LUMPS
 from srctools import FileSystemChain, GameID
+from srctools.filesys import RawFileSystem, VPKFileSystem
+import seecompiler.packlist
 
 import seecompiler.run
 
@@ -16,6 +21,18 @@ import seecompiler.run
 def main(argv):
     LOGGER.info('SEEcompiler VRAD hook started!')
     vrad_args = argv[1:]
+
+    # TODO: extract this and the game ID from gameinfo.txt
+    fsys = FileSystemChain(
+        VPKFileSystem('../portal2_dlc2/pak01_dir.vpk'),
+        VPKFileSystem('../portal2_dlc1/pak01_dir.vpk'),
+        VPKFileSystem('../portal2/pak01_dir.vpk'),
+        RawFileSystem('../portal2_dlc2/'),
+        RawFileSystem('../portal2_dlc1/'),
+        RawFileSystem('../portal2/'),
+        RawFileSystem('../bee2_dev/'),
+        RawFileSystem('../bee2/'),
+    )
 
     # The path is the last argument to VRAD
     # Hammer adds wrong slashes sometimes, so fix that.
@@ -33,14 +50,27 @@ def main(argv):
     LOGGER.info('Reading BSP...')
     bsp_file = BSP(path)
     bsp_file.read_header()
+    bsp_file.read_game_lumps()
+
+    LOGGER.info('Reading entities...')
     vmf = bsp_file.read_ent_data()
     LOGGER.info('Done!')
 
     LOGGER.info('Running transformations...')
-    run_transformations(vmf, FileSystemChain(), GameID.PORTAL_2)
+    run_transformations(vmf, fsys, GameID.PORTAL_2)
 
     bsp_file.write_ent_data(vmf)
     LOGGER.info('Finished writing entities.')
+
+    seecompiler.packlist.pack_from_bsp(bsp_file)
+    seecompiler.packlist.eval_dependencies(fsys)
+
+    pak_file = io.BytesIO(bsp_file.get_lump(BSP_LUMPS.PAKFILE))
+
+    with ZipFile(pak_file, 'a') as pak_zip:
+        seecompiler.packlist.pack_into_zip(fsys, pak_zip)
+
+    bsp_file.replace_lump(bsp_file.filename, BSP_LUMPS.PAKFILE, pak_file.getvalue())
 
     LOGGER.info("SEEcompiler VRAD hook finished!")
 
