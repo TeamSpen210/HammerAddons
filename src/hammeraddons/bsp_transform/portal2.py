1af3282cb959acce4e27ba8ae9cc6c8b87038aed
TeamSpen210 <spencerb21@live.com>
2018-07-01 17:10:04 +1000

    Automatically detect paint present in a map
    Transferred from TeamSpen210/srctools @ 4202fb9702795a80392a61a0d84867fd4ded2425

--- src/hammeraddons/bsp_transform/portal2.py
+++ src/hammeraddons/bsp_transform/portal2.py
@@ -1,6 +1,6 @@
 """Portal-2 specific transformations."""
 from srctools.bsp_transform import trans, Context
-from srctools import Output, conv_bool
+from srctools import Output, conv_bool, conv_int, VMF
 
 
 @trans('Fix Laser Catcher Skins')
@@ -30,3 +30,43 @@ def laser_catcher_skins(ctx: Context):
             ent.add_out(Output('OnPowered', '!self', 'Skin', act_skin))
         if not has_deact:
             ent.add_out(Output('OnUnPowered', '!self', 'Skin', deact_skin))
+
+
+def needs_paint(vmf: VMF) -> bool:
+    """Check if we have paint."""
+    for ent_cls in [
+        'prop_paint_bomb',
+        'paint_sphere',
+        'weapon_paintgun',  # Not in retail but someone might add it.
+    ]:
+        if vmf.by_class[ent_cls]:
+            return True
+
+    for ent in vmf.by_class['info_paint_sprayer']:
+        # Special case, this makes sprayers only render visually, which
+        # works even without the value set.
+        if not conv_bool(ent['DrawOnly']):
+            return True
+
+    for ent_cls in [
+        'prop_weighted_cube',
+        'prop_physics_paintable',
+    ]:
+        for ent in vmf.by_class[ent_cls]:
+            # If the cube is bouncy, enable paint.
+            if conv_int(ent['paintpower', '4'], 4) != 4:
+                return True
+
+
+@trans('Force Paint in Map')
+def force_paintinmap(ctx: Context):
+    """If paint entities are present, set paint in map to true."""
+    # Already set, don't bother confirming.
+    if conv_bool(ctx.vmf.spawn['paintinmap']):
+        return
+
+    if needs_paint(ctx.vmf):
+        ctx.vmf.spawn['paintinmap'] = '1'
+        # Ensure we have some blobs.
+        if conv_int(ctx.vmf.spawn['maxblobcount']) == 0:
+            ctx.vmf.spawn['maxblobcount'] = '250'
