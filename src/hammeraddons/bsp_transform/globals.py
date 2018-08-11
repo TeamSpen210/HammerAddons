c5235fff7e1f0dd787d93ba7f8704a7caa57b967
TeamSpen210 <spencerb21@live.com>
2018-08-11 12:52:16 +1000

    Add Vscript injection logic
    This adds an init code keyvalue, and RunScriptCode quoting functionality
    Transferred from TeamSpen210/srctools @ 987d1fb63b8e8c04108267a0e3fbdd773774f712

--- src/hammeraddons/bsp_transform/globals.py
+++ src/hammeraddons/bsp_transform/globals.py
@@ -24,3 +24,44 @@ def att_points(ctx: Context):
             continue
 
         ent['parentname'] = parent + ',' + ent['parent_attachment_point']
+
+
+@trans('VScript Init Code')
+def vscript_init_code(ctx: Context):
+    """Add vscript_init_code keyvalues.
+
+    The specified code is appended as a script file to the end of the scripts.
+    """
+    for ent in ctx.vmf.entities:
+        code = ent['vscript_init_code']
+        if not code:
+            continue
+        init_scripts = ent['vscripts'].split()
+        init_scripts.append(ctx.pack.inject_file(
+            code.replace('`', '"').encode('utf8'),
+            'scripts/vscripts/inject',
+            'nut'
+        )[17:])  # Don't include scripts/vscripts/
+        ent['vscripts'] = ' '.join(init_scripts)
+        del ent['vscript_init_code']
+
+
+@trans('VScript RunScriptCode Strings')
+def vscript_runscriptcode_strings(ctx: Context):
+    """Allow writing strings in RunScriptCode inputs.
+
+    This is done by using ` as a replacement for double-quotes,
+    then synthesising a script file and using RunScriptFile to execute it.
+    """
+    for ent in ctx.vmf.entities:
+        for out in ent.outputs:
+            if out.input.casefold() != 'runscriptcode':
+                continue
+            if '`' not in out.params:
+                continue
+            out.params = ctx.pack.inject_file(
+                out.params.replace('`', '"').encode('utf8'),
+                'scripts/vscripts/inject',
+                'nut'
+            )[17:]  # Don't include scripts/vscripts/
+            out.input = 'RunScriptFile'
