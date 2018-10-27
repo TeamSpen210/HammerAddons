d216483f691f4531de1ef752c7663fda85f8198b
TeamSpen210 <spencerb21@live.com>
2018-10-27 12:18:00 +1000

    Compile autocombine props
    Skin isn't honoured, and not packed in yet.
    
    Transferred from TeamSpen210/srctools @ 92944ee57d3ee38b53dbba220b726d6e93647bd3

--- src/hammeraddons/propcombine.py
+++ src/hammeraddons/propcombine.py
@@ -3,10 +3,12 @@
 This merges static props together, so they can be drawn with a single
 draw call.
 """
-import re
+import subprocess
 from pathlib import Path
+from tempfile import TemporaryDirectory
 from typing import Dict, List, Iterator, Optional
 
+from srctools import Vec
 from srctools.tokenizer import Tokenizer, Token
 
 from srctools.game import Game
@@ -28,9 +30,52 @@ QC = namedtuple('QC', [
     'phy_scale',  # Scale of collision model.
 ])
 
+QC_TEMPLATE = '''\
+$staticprop
+$modelname "{path}"
+$surfaceprop "{surf}"
+$cdmaterials {cdmats}
+
+$body body blank.smd
+
+$sequence idle blank act_idle 1
+'''
+
+QC_COLL_TEMPLATE = '''
+$collisionmodel "blank" {
+    $maxconvexpieces 64
+    $automass
+    $remove2d
+    $concave
+'''
+
+# No bones, no animation, no geometry...
+BLANK_SMD  = b'''\
+version 1
+nodes
+  0 "static_prop" -1
+end
+skeleton
+  time 0
+    0 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000
+end
+triangles
+end
+'''
+
 
 class DynamicModel(Exception):
     """Used as flow control."""
+
+
+def unify_mdl(path: str):
+    """Compute a 'canonical' path for a given model."""
+    path = path.casefold().replace('\\', '/')
+    if not path.startswith('models/'):
+        path = 'models/' + path
+    if not path.endswith('.mdl'):
+        path = path + '.mdl'
+    return path
 
 
 def load_qcs(game: Game) -> Dict[str, QC]:
@@ -99,7 +144,7 @@ def load_qcs(game: Game) -> Dict[str, QC]:
             # Malformed...
             continue
 
-        qc_map[model_name.casefold()] = QC(
+        qc_map[unify_mdl(model_name)] = QC(
             str(qc_path).replace('\\', '/'),
             str(ref_smd).replace('\\', '/'),
             str(phy_smd).replace('\\', '/'),
@@ -107,39 +152,134 @@ def load_qcs(game: Game) -> Dict[str, QC]:
             phy_scale,
         )
     return qc_map
+
+
+def merge_props(
+    qc_map: Dict[str, QC],
+    mdl_map: Dict[str, Model],
+    game: Game,
+    map_name: str,
+    props: List[StaticProp],
+    counter: int,
+) -> StaticProp:
+    """Given a set of props, merge them together to produce a new prop."""
+
+    center_pos = Vec()
+    for prop in props:
+        center_pos += prop.origin
+    center_pos = round(center_pos / len(props))  # type: Vec
+
+    surfprops = set()
+    cdmats = set()
+    for prop in props:
+        mdl = mdl_map[unify_mdl(prop.model)]
+        surfprops.add(mdl.surfaceprop.casefold())
+        cdmats.update(mdl.cdmaterials)
+
+    if len(surfprops) > 1:
+        raise ValueError('Multiple surfaceprops? Should be filtered out.')
+
+    [surfprop] = surfprops
+
+    if counter > 0xFFFF:
+        raise ValueError('More than 65K models, how??')
+
+    prop_name = 'models/maps/{}/propcombine/merge_{:04X}.mdl'.format(map_name, counter)
+
+    with TemporaryDirectory(prefix='autocomb_') as temp_dir:
+        with open(temp_dir + '/blank.smd', 'wb') as f:
+            f.write(BLANK_SMD)
+        with open(temp_dir + '/model.qc', 'w') as f:
+            f.write(QC_TEMPLATE.format(
+                path=prop_name,
+                surf=surfprop,
+                cdmats=' '.join('"{}"'.format(mat) for mat in sorted(cdmats))
+            ))
+
+            has_coll = False
+
+            for prop in props:
+                qc = qc_map[unify_mdl(prop.model)]
+                f.write(
+                    '$appendsource "{}" "offset '
+                    'pos[ {:.3f} {:.3f} {:.3f} ] '
+                    'angle[ {:.3f} {:.3f} {:.3f} ] '
+                    'scale[ {:.3f} '
+                    ']"\n'.format(
+                        qc.ref_smd,
+                        prop.origin.x - center_pos.x,
+                        prop.origin.y - center_pos.y,
+                        prop.origin.z - center_pos.z,
+                        prop.angles.x,
+                        prop.angles.y,
+                        prop.angles.z,
+                        qc.ref_scale
+                    )
+                )
+                if qc.phy_smd:
+                    has_coll = True
+
+            if has_coll:
+                f.write(QC_COLL_TEMPLATE)
+                for prop in props:
+                    qc = qc_map[unify_mdl(prop.model)]
+                    if qc.phy_smd:
+                        f.write(
+                            '    $addconvexsrc "{}" "offset '
+                            'pos[ {:.3f} {:.3f} {:.3f} ] '
+                            'angle[ {:.3f} {:.3f} {:.3f} ] '
+                            'scale[ {:.3f} '
+                            ']"\n'.format(
+                                qc.ref_smd,
+                                prop.origin.x - center_pos.x,
+                                prop.origin.y - center_pos.y,
+                                prop.origin.z - center_pos.z,
+                                prop.angles.x,
+                                prop.angles.y,
+                                prop.angles.z,
+                                qc.ref_scale
+                            )
+                        )
+                f.write('}\n')
+        args = [
+            'F:/Git/Desolation/game/bin/win32/studiomdl.exe',
+            '-nop4',
+            '-game', str(game.path), temp_dir + '/model.qc',
+        ]
+        subprocess.run(args)
+
+
 def combine(
     bsp: BSP,
     pack: PackList,
     game: Game,
 ):
-    """Combine props in this map.
-
-    temp_path is a location to copy
-    """
+    """Combine props in this map."""
     # Parse through all the QC files.
     LOGGER.info('Parsing QC files...')
     qc_map = load_qcs(game)
     LOGGER.info('Done! {} props.', len(qc_map))
 
-    # Don't re-parse models continually.
-    cache = {}  # type: Dict[str, Model]
+    map_name = Path(bsp.filename).stem
 
-    def get_grouping_key(
-        prop: StaticProp,
-    ) -> object:
+    # Don't re-parse models continually.
+    mdl_map = {}  # type: Dict[str, Model]
+
+    def get_grouping_key(prop: StaticProp) -> object:
         """Compute a grouping key for this prop.
 
         Only props with matching key can be possibly combined.
         """
+        key = unify_mdl(prop.model)
         try:
-            model = cache[prop.model]
+            model = mdl_map[key]
         except KeyError:
             try:
                 mdl_file = pack.fsys[prop.model]
             except FileNotFoundError:
                 # We don't have this model, we can't combine...
                 return None
-            model = cache[prop.model] = Model(pack.fsys, mdl_file)
+            model = mdl_map[key] = Model(pack.fsys, mdl_file)
 
         return (
             model.flags,
@@ -152,6 +292,39 @@ def combine(
         )
 
     # First, construct groups of props that can possibly be combined.
-    prop_groups = defaultdict(list)
+    prop_groups = defaultdict(list)  # type: Dict[object, List[StaticProp]]
     for prop in bsp.static_props():
         prop_groups[get_grouping_key(prop)].append(prop)
+
+    LOGGER.info('Prop combine: \n')
+    for group, props in prop_groups.items():
+        LOGGER.info(group)
+        LOGGER.info('\n'.join(map(repr, props)))
+
+    # Don't worry about distance.
+
+    final_props = []
+    for ind, (key, props) in enumerate(prop_groups.items()):
+        # First remove props that aren't actually mergeable.
+        if key is None:
+            final_props.extend(props)
+            continue
+        for prop in props[:]:
+            if unify_mdl(prop.model) not in qc_map:
+                final_props.append(prop)
+                props.remove(prop)
+
+        if not props:
+            continue
+
+        # Grab the first prop's model file, they'll all have the same settings.
+        rep_model = mdl_map[props[0].model]
+
+        final_props.append(merge_props(
+            qc_map,
+            mdl_map,
+            game,
+            map_name,
+            props,
+            ind,
+        ))
