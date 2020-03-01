33749617f3efb5634dc2617e8dd761d01dc0466c
TeamSpen210 <spencerb21@live.com>
2020-03-01 11:13:04 +1000

    Upgrade comp_trigger_p2_goo to also work for bottomless pits
    Transferred from TeamSpen210/srctools @ 4f0fa55cb9811ce9e1dff4624dc1909321f97a79

--- src/hammeraddons/bsp_transform/brush_ents.py
+++ src/hammeraddons/bsp_transform/brush_ents.py
@@ -1,6 +1,8 @@
 """Implements various brush entities."""
+from typing import Tuple, Dict
+
 from srctools.bsp_transform import trans, Context
-from srctools import Output, conv_bool, conv_float
+from srctools import Output, conv_bool, conv_float, Vec, Entity
 
 
 @trans('P2 Coop Trigger')
@@ -61,13 +63,14 @@ def comp_trigger_coop(ctx: Context):
 @trans('P2 Goo')
 def comp_trigger_goo(ctx: Context):
     """Creates triggers for Toxic Goo."""
+    reloader_cache = {}  # type: Dict[Tuple[float, float, float, float], Entity]
+
     for trig in ctx.vmf.by_class['comp_trigger_p2_goo']:
         trig.remove()
         outputs = trig.outputs.copy()
         trig.outputs.clear()
 
         failsafe_delay = conv_float(trig['failsafe_delay'], 0.5)
-        del trig['failsafe_delay']
         if failsafe_delay < 0.01:
             failsafe_delay = 0.01
 
@@ -75,28 +78,67 @@ def comp_trigger_goo(ctx: Context):
         diss = trig.copy()
         ctx.vmf.add_ents([hurt, diss])
 
-        hurt['classname'] = 'trigger_hurt'
-        hurt['damagetype'] = 262144  # Radiation
-        hurt['damage'] = hurt['damagecap'] = 10000
-        hurt['damagemodel'] = 0  # No doubling
-        hurt['nodmgforce'] = 1  # Don't throw players around.
-        hurt['spawnflags'] = 1  # Players.
-        del hurt['filtername']
+        for keyvalue in [
+            'dissolve_filter',
+            'phys_offset',
+            'failsafe_delay',
+            'fadepreset', 'fadecolor', 'fadetime',
+        ]:
+            del diss[keyvalue], hurt[keyvalue]
 
         diss['classname'] = 'trigger_multiple'
         diss['spawnflags'] = 1096  # Physics, physics debris, everything
         diss['wait'] = 0  # No delay.
         diss['filtername'] = trig['dissolve_filter']
+        del diss['damagetype']
+
+        diss_pos = Vec.from_str(diss['origin'])
+        diss_pos.z -= conv_float(trig['phys_offset'])
+        diss['origin'] = diss_pos
+
+        hurt['spawnflags'] = 1  # Players.
+
+        if conv_bool(trig['enablefade']):
+            fade_time = conv_float(trig['fadetime'])
+            fade_color = Vec.from_str(trig['fadepreset'])
+            if fade_color == (-1, -1, -1):
+                fade_color = Vec.from_str(trig['fadecolor'])
+            fade_key = fade_color.x, fade_color.y, fade_color.z, fade_time
+            try:
+                reloader = reloader_cache[fade_key]
+            except KeyError:
+                reloader = reloader_cache[fade_key] = ctx.vmf.create_ent(
+                    'player_loadsaved',
+                    origin=diss['origin'],
+                    rendercolor=str(fade_color),
+                    renderamt=255,
+                    duration=fade_time,
+                    holdtime=10,
+                    loadtime=fade_time + 0.1,
+                )
+                reloader.make_unique('reloader')
+            hurt['classname'] = 'trigger_once'
+            del hurt['damagetype']
+            hurt.add_out(Output('OnStartTouch', reloader, 'Reload', only_once=True))
+
+            # Make sure the failsafe delay is longer than the total fade time.
+            failsafe_delay = min(failsafe_delay, fade_time + 0.15)
+        else:
+            hurt['classname'] = 'trigger_hurt'
+            hurt['damage'] = hurt['damagecap'] = 10000
+            hurt['damagemodel'] = 0  # No doubling
+            hurt['nodmgforce'] = 1  # Don't throw players around.
+
+        for out in outputs:
+            if out.output.casefold() == 'onkillplayer':
+                # Better than OnStartTouch, doesn't apply for god mode.
+                out.output = 'OnHurtPlayer'
+                hurt.add_out(out)
+            elif out.output.casefold() == 'ondissolvephysics':
+                out.output = 'OnStartTouch'
+                diss.add_out(out)
 
         diss.add_out(
             Output('OnStartTouch', '!activator', 'SilentDissolve'),
             Output('OnStartTouch', '!activator', 'Kill', delay=failsafe_delay),
         )
-
-        for out in outputs:
-            if out.output.casefold() == 'onkillplayer':
-                out.output = 'OnStartTouch'
-                hurt.add_out(out)
-            elif out.output.casefold() == 'ondissolvephysics':
-                out.output = 'OnStartTouch'
-                diss.add_out(out)
