9ceafd0202b7efced3d6911805b69032f82fc955
TeamSpen210 <spencerb21@live.com>
2020-10-08 16:44:03 +1000

    Tweaks to plugin code
    - Use FileNotFoundError
    - Switch to LOGGER (__dunder__ names are reserved for Python itself)
    - Keep a reference to the module involved
    
    Transferred from TeamSpen210/srctools @ afae914d21b7b18d2956089cfde021fac543a318

--- src/hammeraddons/plugin.py
+++ src/hammeraddons/plugin.py
@@ -1,31 +1,36 @@
+import types
 from pathlib import Path
 from importlib.util import spec_from_file_location, module_from_spec
+from typing import Optional
 
 from srctools.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
+
 class Plugin:
     """A plugin loaded by the postcompiler.
 
-    This loads a module, and gives it a logger at __srctools_logger__
+    This loads a module, and gives it a logger at LOGGER
     """
     def __init__(self, path: Path) -> None:
         self.path = path
+        self.module: Optional[types.ModuleType] = None
 
     def load(self) -> None:
+        """Load and execute the module."""
         name = self.path.stem
         spec = spec_from_file_location(name, self.path)
 
         if not spec:
-            raise ValueError('Plugin {} not found at "{}"'.format(name, self.path))
+            raise FileNotFoundError('Plugin {} not found at "{}"'.format(name, self.path))
 
-        mod = module_from_spec(spec)
+        self.module = module_from_spec(spec)
 
         logname = '.' + name
-        #this logger is put into the plugin so it has a consistant log name of "plugin.<plugin name>"
-        mod.__srctools_logger__ = get_logger(__name__ + logname, "plugin" + logname)
+        # Provide a logger for the plugin, already setup.
+        self.module.LOGGER = get_logger(__name__ + logname, "plugin" + logname)
 
-        spec.loader.exec_module(mod)
+        spec.loader.exec_module(self.module)
 
         LOGGER.info('Loaded plugin "{}" ({})', name, self.path)
