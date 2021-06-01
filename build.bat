7eb8e3e09cc424fbd12fb3d83676561b6399a206
brae <brae@momentum-mod.org>
2021-06-01 03:29:48 -0700

    feat: new build folder structure
    This brings the folder structure more in line of that of chaos games, so this can be effectively be copied over completely. The workflow has been modified to now upload everything.

--- build.bat
+++ build.bat
@@ -1,39 +1,67 @@
 @echo off
+SETLOCAL enabledelayedexpansion
 
 SET games=momentum p2ce
-SET game=%1
 
-:: Make sure game isn't empty
+SET "build_dir=build"
+SET bin_dir=bin/win64
+
+SET game=%1
+:: Make sure game isn't empty by asking the user for what game to build
 :while
 IF [%game%]==[] (echo Games: %games% & echo Enter game to build. Use ALL to build every game. & SET /P game= & GOTO :while)
 
+echo Removing previous build in ./%build_dir%/
+rmdir /S /Q "%build_dir%"
+
 IF /I %game%==ALL (
-  CALL :copy_hammer_files
-  (FOR %%i in (%games%) do (
-    CALL :build_game "%%i"
-  ))
+  :: Modify build directory to not have directories clash
+  CALL SET "main_build_dir=!build_dir!"
+  FOR %%i in (%games%) do (
+    CALL SET "game_build_dir=!main_build_dir!"
+    SET "build_dir=!game_build_dir!/%%i"
+    CALL :build_%%i
+  )
   EXIT
 ) ELSE (
-  (FOR %%i in (%games%) do (
+  FOR %%i in (%games%) do (
     IF /I %game%==%%i (
-      CALL :copy_hammer_files
-      CALL :build_game %game%
+      CALL :build_%%i
       EXIT
     )
-  ))
+  )
   echo Unknown game. Exitting. & EXIT /B 1
 )
 
-:build_game
+:build_p2ce
+  CALL :copy_hammer_files p2ce
+  CALL :copy_postcompiler_files
+  CALL :build_game_fgd p2ce
+  EXIT /B
+
+:build_momentum
+  CALL :copy_hammer_files momentum
+  CALL :build_game_fgd momentum
+  EXIT /B
+
+:build_game_fgd
   echo Building FGD for %1...
-  python unify_fgd.py exp %1 srctools -o "build/%1.fgd"
+  mkdir "%build_dir%/%1"
+  python unify_fgd.py exp %1 srctools -o "%build_dir%/%1/%1.fgd"
+
   IF %ERRORLEVEL% NEQ 0 (echo Building FGD for %1 has failed. Exitting. & EXIT)
   EXIT /B
 
 :copy_hammer_files
   echo Copying Hammer files...
-  IF %ERRORLEVEL% LSS 8 robocopy hammer build/hammer /S /PURGE
-  IF %ERRORLEVEL% LSS 8 robocopy instances build/instances /XF *.vmx /S /PURGE
-  IF %ERRORLEVEL% LSS 8 robocopy transforms build/postcompiler/transforms /PURGE
+  robocopy hammer %build_dir%/hammer /S /PURGE
+
   IF %ERRORLEVEL% LSS 8 EXIT /B 0
-  echo Failed copying Hammer files. Exitting. & EXIT
+  echo Failed copying Hammer files for %1. Exitting. & EXIT
+
+:copy_postcompiler_files
+  echo Copying postcompiler transforms...
+  robocopy transforms %build_dir%/%bin_dir%/postcompiler/transforms /S /PURGE
+  
+  IF %ERRORLEVEL% LSS 8 EXIT /B 0
+  echo Failed copying postcompiler transforms. Exitting. & EXIT
--- build.sh
+++ build.sh
@@ -1,46 +1,77 @@
 #!/bin/sh
+
 games="momentum p2ce"
+
+build_dir="build"
+bin_dir="bin/win64" # Hammer is windows only
+
 game=$1
+# Make sure game isn't empty by asking the user for what game to build
 if [ $# -eq 0 ]; then
   echo Games: "${games[*]}" & echo Enter game to build. Use ALL to build every game. & read -p "" game
 fi
 
+echo "Removing previous build in $build_dir"
+rm -rf "$build_dir"
+
+build_p2ce() {
+  copy_hammer_files p2ce
+  copy_postcompiler_files
+  build_game_fgd p2ce
+}
+
+build_momentum() {
+  copy_hammer_files momentum
+  build_game_fgd momentum
+}
+
 copy_hammer_files() {
   echo "Copying Hammer files..."
-  mkdir -p build/postcompiler &&
-  cp -rf hammer build/hammer &&
-  cp -rf instances build/instances &&
-  cp -rf transforms build/postcompiler/transforms &&
-  find ./build/instances -iname "*.vmx" -delete # Yes, I know that we could use rsync with a ton of options to do this instead of using cp and then deleting unwanted files. This is FAR nicer imo.
-  
+  mkdir -p "$build_dir"
+  rsync -a "hammer" "$build_dir"
+
   if [ $? -ne 0 ]; then
     echo "Failed copying Hammer files. Exitting." & exit 1
   fi
   return 0
 }
 
-build_game() {
+build_game_fgd() {
   echo "Building FGD for $1..."
-  python3 unify_fgd.py exp $1 srctools -o "build/$1.fgd"
-  
+  mkdir -p "$build_dir/$1"
+  python3 unify_fgd.py exp $1 srctools -o "$build_dir/$1/$1.fgd"
+
   if [ $? -ne 0 ]; then
     echo "Building FGD for $1 has failed. Exitting." & exit 1
   fi
   return 0
 }
 
-if [ "${game^^}" = "ALL" ]; then 
-  copy_hammer_files
-  for i in $games 
+copy_postcompiler_files() {
+  echo "Copying postcompiler transforms..."
+  mkdir -p "$build_dir/$bin_dir/postcompiler"
+  cp -rf transforms "$build_dir/$bin_dir/postcompiler"
+
+  if [ $? -ne 0 ]; then
+    echo "Failed copying postcompiler transforms. Exitting." & exit 1
+  fi
+  return 0
+}
+
+if [ "${game^^}" = "ALL" ]; then
+  # Modify build directory to not have directories clash
+  main_build_dir=$build_dir
+  for i in $games
     do
-    build_game $i
+    unset build_dir
+    build_dir=$main_build_dir/$i
+    build_$i
   done
 else
   for i in $games
     do
-    if [ "$i" = "$game" ]; then 
-      copy_hammer_files
-      build_game $game
+    if [ "$i" = "$game" ]; then
+      build_$game
       exit
     fi
     echo "Unknown game. Exitting." & exit 1
