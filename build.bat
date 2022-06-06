6a7de5b1da08f679bb2a9b5a373113ce4bbd1fc5
braem <brae@momentum-mod.org>
2022-06-05 22:44:08 -0600

    feat: add md gen to build scripts

--- build.bat
+++ build.bat
@@ -2,8 +2,10 @@
 SETLOCAL enabledelayedexpansion
 
 SET games=momentum p2ce
+SET modes=fgd md
 
 SET "build_dir=build"
+SET "build_md_dir=build_md"
 SET bin_dir=bin/win64
 
 :: Setup hammer folder copy exclusions (*_momentum, *_p2ce, etc)
@@ -13,41 +15,83 @@ FOR %%i in (%games%) do (
   SET "robocopy_exclusions=!robocopy_exclusions!!game_exclude!"
 )
 
-SET game=%1
+SET mode=%1
+:: Make sure mode isn't empty by asking the user
+:while_mode
+IF [%mode%]==[] (echo Modes: %modes% & echo Enter mode. Use ALL to build everything. & SET /P mode= & GOTO :while_mode)
+
+SET game=%2
 :: Make sure game isn't empty by asking the user for what game to build
-:while
-IF [%game%]==[] (echo Games: %games% & echo Enter game to build. Use ALL to build every game. & SET /P game= & GOTO :while)
+:while_game
+IF [%game%]==[] (echo Games: %games% & echo Enter game to build. Use ALL to build every game. & SET /P game= & GOTO :while_game)
 
-echo Removing previous build in ./%build_dir%/
-rmdir /S /Q "%build_dir%"
+IF /I %mode%==ALL (
+  CALL :build_fgd_cleanup
+  CALL :build_md_cleanup
+  GOTO :main_build_step
+)
+IF /I %mode%==FGD (
+  CALL :build_fgd_cleanup
+  GOTO :main_build_step
+)
+IF /I %mode%==MD (
+  CALL :build_md_cleanup
+  GOTO :main_build_step
+)
 
+:main_build_step
 IF /I %game%==ALL (
   :: Modify build directory to not have directories clash
   CALL SET "main_build_dir=!build_dir!"
   FOR %%i in (%games%) do (
     CALL SET "game_build_dir=!main_build_dir!"
     SET "build_dir=!game_build_dir!/%%i"
-    CALL :build_%%i
+    CALL :build %%i
   )
   EXIT
 ) ELSE (
   FOR %%i in (%games%) do (
     IF /I %game%==%%i (
-      CALL :build_%%i
+      CALL :build %%i
       EXIT
     )
   )
   echo Unknown game. Exitting. & EXIT /B 1
 )
 
-:build_p2ce
+:build_fgd_cleanup
+  echo Removing previous FGD build in ./%build_dir%/
+  rmdir /S /Q "%build_dir%"
+  EXIT /B
+
+:build_md_cleanup
+  echo Removing previous markdown build in ./%build_md_dir%/
+  rmdir /S /Q "%build_md_dir%"
+  EXIT /B
+
+:build
+  IF /I %mode%==ALL (
+    CALL :build_fgd_%%1
+    CALL :build_game_markdown %1
+    EXIT /B
+  )
+  IF /I %mode%==FGD (
+    CALL :build_fgd_%%1
+    EXIT /B
+  )
+  IF /I %mode%==MD (
+    CALL :build_game_markdown %1
+  )
+  EXIT /B
+
+:build_fgd_p2ce
   CALL :copy_hammer_files p2ce
   CALL :copy_vscript_files
   CALL :copy_postcompiler_files
   CALL :build_game_fgd p2ce
   EXIT /B
 
-:build_momentum
+:build_fgd_momentum
   CALL :copy_hammer_files momentum
   CALL :build_game_fgd momentum
   EXIT /B
@@ -58,6 +102,14 @@ IF /I %game%==ALL (
   python unify_fgd.py exp %1 srctools -o "%build_dir%/%1/%1.fgd"
 
   IF %ERRORLEVEL% NEQ 0 (echo Building FGD for %1 has failed. Exitting. & EXIT)
+  EXIT /B
+
+:build_game_markdown
+  echo Generating markdown from FGD for %1...
+  mkdir "%build_md_dir%/%1"
+  python unify_fgd.py expmd %1 srctools -o "%build_md_dir%/%1"
+  
+  IF %ERRORLEVEL% NEQ 0 (echo Building markdown for %1 has failed. Exitting. & EXIT)
   EXIT /B
 
 :copy_hammer_files
--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+modes="fgd md"
 games="momentum p2ce"
 
 build_dir="build"
+build_md_dir="build_md"
 bin_dir="bin/win64" # Hammer is windows only
 
 # Setup hammer folder copy exclusions (*_momentum, *_p2ce, etc)
@@ -12,25 +14,58 @@ for i in $games
   copy_exclusions+=(--exclude="*_$i")
 done
 
-game=$1
+mode=$1
+# Make sure mode isn't empty by asking the user
+if [ $# -eq 0 ]; then
+  echo Modes: "${modes[*]}" && echo Enter mode. Use ALL to build everything. && read -p "" mode
+fi
+
+game=$2
 # Make sure game isn't empty by asking the user for what game to build
 if [ $# -eq 0 ]; then
   echo Games: "${games[*]}" && echo Enter game to build. Use ALL to build every game. && read -p "" game
 fi
 
-echo "Removing previous build in $build_dir"
-rm -rf "$build_dir"
+build_fgd_cleanup() {
+  echo "Removing previous FGD build in $build_dir"
+  rm -rf "$build_dir"
+}
+build_md_cleanup() {
+  echo "Removing previous markdown build in $build_md_dir"
+  rm -rf "$build_md_dir"
+}
 
-build_p2ce() {
+build() {
+  if [[ "${mode^^}" = "FGD" ]] || [[ "${mode^^}" = "ALL" ]]; then
+    build_fgd_$1
+  fi
+  if [[ "${mode^^}" = "MD" ]] || [[ "${mode^^}" = "ALL" ]]; then
+    build_game_markdown $1
+  fi
+  return 0
+}
+
+build_fgd_p2ce() {
   copy_hammer_files p2ce
   copy_vscript_files
   copy_postcompiler_files
   build_game_fgd p2ce
 }
 
-build_momentum() {
+build_fgd_momentum() {
   copy_hammer_files momentum
   build_game_fgd momentum
+}
+
+build_game_markdown() {
+  echo "Generating markdown from FGD for $1..."
+  mkdir -p "$build_md_dir/$1"
+  python3 unify_fgd.py expmd $1 srctools -o "$build_md_dir/$1"
+
+  if [ $? -ne 0 ]; then
+    echo "Building markdown for $1 has failed. Exitting." && exit 1
+  fi
+  return 0
 }
 
 copy_hammer_files() {
@@ -78,6 +113,13 @@ copy_postcompiler_files() {
   return 0
 }
 
+if [[ "${mode^^}" = "FGD" ]] || [[ "${mode^^}" = "ALL" ]]; then
+  build_fgd_cleanup
+fi
+if [[ "${mode^^}" = "MD" ]] || [[ "${mode^^}" = "ALL" ]]; then
+  build_md_cleanup
+fi
+
 if [ "${game^^}" = "ALL" ]; then
   # Modify build directory to not have directories clash
   main_build_dir=$build_dir
@@ -85,13 +127,13 @@ if [ "${game^^}" = "ALL" ]; then
     do
     unset build_dir
     build_dir=$main_build_dir/$i
-    build_$i
+    build $i
   done
 else
   for i in $games
     do
     if [ "$i" = "$game" ]; then
-      build_$game
+      build $game
       exit
     fi
   done
