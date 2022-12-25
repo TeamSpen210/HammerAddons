0e4a00d181b88420ff919cee301936f29992d170
Spencer Brown <spencerb21@live.com>
2022-12-25 19:08:44 +1000

    Add a bit of infrastructure for testing transforms

---
+++ tests/test_transforms/__init__.py
@@ -0,0 +1,40 @@
+import importlib
+import sys
+from pathlib import Path
+from typing import Awaitable, Callable
+
+import pytest
+
+from hammeraddons.bsp_transform import Context, TransFunc, TRANSFORMS
+from srctools.bsp import BSP
+from srctools.filesys import VirtualFileSystem
+from srctools.game import Game
+from srctools.packlist import PackList
+
+
+@pytest.fixture
+def blank_ctx(shared_datadir: Path) -> Context:
+    """Build a blank context."""
+    bsp = BSP(shared_datadir / 'blank.bsp')
+    game = Game(shared_datadir)
+    fsys = VirtualFileSystem({})
+    return Context(
+        fsys,
+        bsp.ents,
+        PackList(fsys),
+        bsp,
+        game,
+    )
+
+
+def get_transform_func(module_name: str, transform: str) -> Callable[[Context], Awaitable[None]]:
+    """Import the builtin transforms, then fetch this context."""
+    folder = str(Path(__file__, '..', '..', '..', 'transforms').resolve())
+    print(f'Adding "{folder}" to path.')
+
+    sys.path.append(folder)
+    try:
+        importlib.import_module(module_name)
+        return TRANSFORMS[transform]
+    finally:
+        sys.path.remove(folder)
