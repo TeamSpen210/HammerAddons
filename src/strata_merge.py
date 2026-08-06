0c09aec6bf1522dd72ef9d51b332ea1c286a058a
Enderek <enderek72@gmail.com>
2026-08-06 18:12:57 +0200

    change: Update strata_merge.py

--- src/strata_merge.py
+++ src/strata_merge.py
@@ -9,80 +9,21 @@ from srctools import FGD
 from sys import argv
 
 MERGED = {  # Set of classnames we have checked already and know the diff is fine.
-    'baseentityvisbrush', 'baselight', 'baseentityinputs', 'baseentityphysics', 'basepropphysics',
-    'fadedistance', 'gibshooterbase', 'grenadeuser', 'basebeam', 'baselogicalnpc', 'basenpc',
-    'basespotlight', 'button', 'door', 'followgoal', 'hintnode',
-    'baseheadcrab', 'basehelicopter', 'baseportbutton', 'basetrain', 'combinescanner', 'damagetype',
+    "info_player_start", "env_soundscape", "momentary_rot_button",
+    "ai_script_conditions", "logic_script", "trigger_hierarchy",
+    "prop_under_button", "npc_bullseye", "paint_sphere",
+    "damagetype", "env_sprite", "info_landmark_entry", "info_landmark_exit",
+    "env_alyxemp", "env_laser", "logic_timer", 
 
-    'ai_changetarget', 'ai_goal_actbusy', 'ai_goal_injured_follow', 'scripted_scene',
-    'ai_script_conditions', 'aiscripted_schedule', 'ambient_generic', 'keyframe_track',
-    'monster_generic', 'move_keyframed', 'bounce_bomb', 'func_instance_origin', 'test_sidelist',
-    'test_traceline', 'color_correction_volume', 'combine_mine', 'cycler', 'game_weapon_manager',
-    'color_correction', 'game_score', 'generic_actor', 'grenade_helicopter',
-    'comp_adv_output', 'comp_case', 'comp_entity_finder', 'comp_kv_setter', 'comp_prop_cable',
-    'comp_prop_cable_dynamic', 'comp_prop_rope', 'comp_prop_rope_dynamic', 'comp_propcombine_set',
-    'comp_propcombine_volume', 'comp_relay', 'comp_sequential_call', 'comp_trigger_coop',
-    'comp_vactube_start', 'comp_vactube_junction', 'comp_vactube_end', 'comp_vactube_sensor',
-    'comp_vactube_spline', 'comp_vactube_object', 'comp_piston_platform', 'comp_movie_fitter',
-    'comp_multi_command', 'comp_trigger_p2_goo', 'hammer_model', 'hammer_notes',
-    'env_effectscript', 'env_fade', 'env_fire', 'env_fog_controller',
-    'env_projectedtexture', 'env_portal_laser', 'env_soundscape', 'env_rockettrail', 'env_sun',
-    'env_rotorwash_emitter', 'env_blood', 'env_bubbles', 'env_embers', 'env_starfield', 'env_steam',
-    'env_zoom', 'env_ar2explosion', 'env_explosion', 'env_shooter', 'env_smoketrail', 'env_speaker',
-    'env_sprite', 'env_tonemap_controller', 'env_wind', 'env_microphone', 'env_movieexplosion',
-    'env_global', 'env_headcrabcanister', 'env_screenoverlay', 'env_player_viewfinder',
-    'env_particlescript', 'env_credits',
-    'hot_potato', 'hot_potato_catcher', 'hot_potato_socket', 'hot_potato_spawner',
-    'filter_enemy', 'filter_multi', 'func_areaportal', 'func_breakable', 'func_breakable_surf',
-    'filter_activator_class', 'filter_activator_involume', 'filter_damage_type',
-    'filter_activator_team', 'filter_activator_keyfield', 'filter_activator_model',
-    'func_brush', 'func_combine_ball_spawner', 'func_door', 'func_illusionary', 'func_physbox',
-    'func_instance', 'func_instance_io_proxy', 'func_clip_vphysics', 'func_tankairboatgun',
-    'func_movelinear', 'linked_portal_door', 'func_smokevolume', 'func_tanklaser', 'func_tankmortar',
-    'func_tankphyscannister', 'func_door_rotating', 'func_placement_clip', 'func_proprrespawnzone',
-    'func_precipitation', 'func_water_analog', 'func_wall_toggle', 'func_wall', 'func_water',
-    'func_dustcloud', 'func_dustmotes',
-    'game_globalvars', 'game_player_team', 'game_text',
-    'item_ammo_357', 'item_ammo_357_large',
-    'item_ammo_ar2', 'item_ammo_ar2_altfire', 'item_ammo_ar2_large', 'item_healthcharger',
-    'item_healthkit', 'item_healthvial', 'item_item_crate', 'item_large_box_lrounds',
-    'item_large_box_mrounds', 'item_large_box_srounds', 'item_rpg_round', 'item_suit',
-    'item_suitcharger', 'item_box_buckshot', 'item_battery', 'item_grubnugget',
-    'item_ammo_crossbow', 'item_ammo_crate', 'item_ammo_pistol', 'item_ammo_pistol_large',
-    'item_ammo_smg1', 'item_ammo_smg1_grenade', 'item_ammo_smg1_large', 'item_ar2_grenade',
-    'info_coop_spawn', 'info_apc_missile_hint', 'info_ladder_dismount',
-    'info_lighting_relative', 'info_npc_spawn_destination', 'info_overlay_transition',
-    'item_box_lrounds', 'item_box_mrounds', 'item_box_srounds', 'info_paint_sprayer',
-    'info_radar_target', 'info_snipertarget', 'info_target_gunshipcrash', 'info_overlay',
-    'info_player_deathmatch', 'physics_cannister', 'point_bugbait', 'point_worldtext',
-    'info_target_vehicle_transition', 'info_target_helicopter_crash', 'info_teleporter_countdown',
-    'info_darknessmode_lightsource', 'info_intermission', 'infodecal',
-    'logic_achievement', 'logic_choreographed_scene', 'logic_compare', 'logic_playerproxy',
-    'logic_timer', 'logic_auto', 'logic_convar', 'logic_gate', 'logic_measure_direction',
-    'logic_measure_movement', 'logic_modelinfo', 'logic_playmovie', 'logic_scene_list_manager',
-    'logic_script', 'logic_sequence', 'material_modify_control', 'logic_case',
-    'logic_eventlistener_itemequip', 'logic_relay',
-    'light_dynamic', 'light_directional',
-    'light', 'light_spot', 'light_rt', 'light_rt_spot', 'light_environment', 'logic_random_outputs',
-    'prop_thumper', 'path_track', 'prop_laser_catcher', 'prop_testchamber_sign', 'prop_tractor_beam',
-    'prop_testchamber_door', 'paint_sphere', 'prop_door_rotating', 'prop_static', 'prop_portal',
-    'prop_dynamic_ornament', 'prop_glados_core', 'prop_physics_ragdoll', 'prop_stickybomb',
-    'point_bonusmaps_accessor', 'point_broadcastclientcommand', 'point_camera', 'point_changelevel',
-    'point_teleport', 'point_viewcontrol', 'point_viewcontrol_multiplayer', 'point_spotlight',
-    'npc_pigeon', 'npc_crow', 'npc_rollermine', 'npc_seagull', 'npc_security_camera',
-    'npc_strider', 'npc_zombine', 'npc_dog', 'npc_eli', 'npc_advisor', 'npc_citizen', 'npc_alyx',
-    'npc_clawscanner', 'npc_combine_s', 'npc_combinedropship', 'npc_combinegunship', 'npc_cscanner',
-    'npc_enemyfinder', 'npc_enemyfinder_combinecannon', 'npc_fastzombie', 'npc_fastzombie_torso',
-    'npc_heli_avoidsphere', 'npc_headcrab_poison', 'npc_headcrab_black', 'npc_grenade_frag',
-    'npc_helicopter', 'npc_metropolice', 'npc_antlion_template_maker', 'npc_bullseye', 'npc_monk',
-    'npc_combine_camera', 'npc_combine_cannon', 'npc_hunter_maker', 'npc_launcher', 'npc_tripmine',
-    'npc_personality_core', 'npc_portal_turret_floor', 'npc_rocket_turret', 'npc_template_maker',
-    'trigger_soundoperator', 'trigger_soundscape', 'trigger_playermovement', 'trigger_playerteam',
-    'trigger_togglesave', 'trigger_remove', 'trigger_rpgfire', 'trigger_tonemap', 'trigger_transition',
-    'trigger_wind', 'trigger_weapon_dissolve', 'trigger_weapon_strip', 'trigger_portal_cleanser',
-    'trigger_proximity', 'trigger_serverragdoll', 'trigger_setspeed', 'trigger_teleport',
-    'trigger_waterydeath',
-    'vgui_movie_display', 'weapon_portalgun', 'momentary_rot_button',
+    "scripted_scene", # Legacy entity
+
+    "light", "light_spot", # I belive that in order to avoid confusion, we should
+    # omit the dynamic settings on `light`
+
+    "light_directional",
+    "prop_portal", "prop_wall_projector", "prop_weighted_cube",
+
+
 }
 
 REPORT_DIR = Path('..', 'strata_merge').resolve()
@@ -137,10 +78,10 @@ def main() -> None:
     all_good_internal1 = True # Used for comparing internal data
 
     #Short aliases for less typing
-    def mdirty():
+    def mdirty(classname):
         nonlocal all_good
         if all_good:
-            print("\n")
+            print(f"Classname: {classname}: \n")
             all_good = False
 
     def mdirty_internal1(msg):
@@ -149,14 +90,14 @@ def main() -> None:
             print(msg)
             all_good_internal1 = False
 
+    classes -= MERGED # remove merged
+
     for classname in classes:
         classname: str
         all_good = True
 
-        if classname.startswith("comp_"): # Skip all postcomp entities
+        if classname.startswith("comp_") or classname.startswith("base"): # Skip all postcomp entities and bases (?)
             continue
-
-        print(f"Checking classname: {classname} ... ", end="")
 
         try:
             pent: EntityDef = strata_fgd[classname]
@@ -171,7 +112,7 @@ def main() -> None:
         try:
             hent: EntityDef = ha_fgd[classname]
         except KeyError:
-            mdirty()
+            mdirty(classname)
             print("|-> New entity (not present in FGD 2)!")
             continue
 
@@ -198,14 +139,17 @@ def main() -> None:
                 if ONLY_MISSING_IN_FGD2:
                     continue
 
-                mdirty()
+                mdirty(classname)
                 print(f"|-> Keyvalue '{key}' definition missing in FGD 1, not implemented?")
                 continue
 
             try:
                 hkv = hent.keyvalues[key]
             except KeyError:
-                mdirty()
+                if key in ("comp_custom_model_type"):
+                    continue
+
+                mdirty(classname)
                 print(f"|-> New keyvalue (not present in FGD 2): '{key}'")
                 continue
 
@@ -220,13 +164,16 @@ def main() -> None:
             # Step 0 - Name checking - already done
             # Step 1 - Kv type
             if pkv._type != hkv._type:
-                mdirty()
+                mdirty(classname)
                 mdirty_internal1(f"|-> Keyvalue '{key}': ")
                 print(f"    |-> Different kv type | FGD 1: {pkv._type} <=> FGD 2: {hkv._type}")
 
             # Step 2 - Default value
             if pkv.default != hkv.default:
-                mdirty()
+                if key in ("fademaxdist", "fademindist", "fadescale", "modelscale", "texframeindex", "shadowcastdist"):
+                    continue
+
+                mdirty(classname)
                 mdirty_internal1(f"|-> Keyvalue '{key}': ")
                 print(f"    |-> Different default value | FGD 1: {pkv.default} <=> FGD 2: {hkv.default}")
 
@@ -241,10 +188,16 @@ def main() -> None:
                 allchoices = set([x.value for x in pkv_choices]) | set([x.value for x in hkv_choices])
 
                 for ch in allchoices:
+                    if ch == "": # I'm not sure what is happening here, but sometimes we get issues where a value like this shows up
+                        continue
+
                     try:
                         pkvch = pkv_choices_d[ch]
                     except KeyError:
-                        mdirty()
+                        if ONLY_MISSING_IN_FGD2:
+                            continue
+                        
+                        mdirty(classname)
                         mdirty_internal1(f"|-> Keyvalue '{key}': ")
                         print(f"    |-> [Choices] Value {ch} does not exist in FGD 1!")
                         continue
@@ -252,7 +205,7 @@ def main() -> None:
                     try:
                         hkvch = hkv_choices_d[ch]
                     except KeyError:
-                        mdirty()
+                        mdirty(classname)
                         mdirty_internal1(f"|-> Keyvalue '{key}': ")
                         print(f"    |-> [Choices] New value: '{ch}' -> '{pkvch.name}'")
                         continue
@@ -260,10 +213,10 @@ def main() -> None:
                     if pkvch.name != hkvch.name:
 
                         # Special cases: Some kvs have their names changed (like grammatical changes)
-                        if key in ("renderfx", "mincpulevel", "mingpulevel", "maxgpulevel", "maxcpulevel"):
+                        if key in ("renderfx", "mincpulevel", "mingpulevel", "maxgpulevel", "maxcpulevel", "teamnum"):
                             continue
 
-                        mdirty()
+                        mdirty(classname)
                         mdirty_internal1(f"|-> Keyvalue '{key}': ")
                         print(f"    |-> [Choices] Different names for value '{ch}' | FGD 1: '{pkvch.name}' | FGD 2: '{hkvch.name}' ")
 
@@ -278,12 +231,12 @@ def main() -> None:
                 if ONLY_MISSING_IN_FGD2:
                     continue
 
-                mdirty()
+                mdirty(classname)
                 print(f" |-> Missing input in FGD 1: {inp}")
                 continue
 
             if not inp in hent.inputs.keys():
-                mdirty()
+                mdirty(classname)
                 print(f"|-> New input: {inp}")
 
         all_outputs_names = set(pent.outputs.keys()) | set(hent.outputs.keys())
@@ -292,19 +245,15 @@ def main() -> None:
             if not inp in pent.outputs.keys():
                 if ONLY_MISSING_IN_FGD2:
                     continue
-                
-                mdirty()
+
+                mdirty(classname)
                 print(f" |-> Missing output in FGD 1: {inp}")
                 continue
 
             if not inp in hent.outputs.keys():
-                mdirty()
+                mdirty(classname)
                 print(f"|-> New output: {inp}")
                 
-
-
-        if all_good:
-            print("All Good!")
 
         print("\n")
         
